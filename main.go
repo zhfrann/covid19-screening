7137a6239652224adebea643d069295fd30bda43
zhfrann <muhzhafranilham13@gmail.com>
2024-05-15 21:22:26 +0700

    feat: create menuMessage()

--- main.go
+++ main.go
@@ -12,32 +12,33 @@ func main() {
 
 	defaultDataPatient(&patient) //aktifkan jika ingin menggunakan data default
 
-	fmt.Println("Menu opsi :")
-	fmt.Println("1. Lihat data pasien\n2. Masukkan data pasien\n")
-	fmt.Print("Pilih opsi : ")
+	menuMessage()
 	fmt.Scan(&option_choose)
 
 	for option_valid {
 		if option_choose == 1 {
 			showData(patient, patients_length)
 
-			fmt.Println("Menu opsi :")
-			fmt.Println("1. Lihat data pasien      2. Masukkan data pasien")
-			fmt.Print("Pilih opsi : ")
+			menuMessage()
 			fmt.Scan(&option_choose)
 
 		} else if option_choose == 2 {
 			insertData(&patient, &patients_length)
 
-			fmt.Println("Menu opsi :")
-			fmt.Println("1. Lihat data pasien      2. Masukkan data pasien")
-			fmt.Print("Pilih opsi : ")
+			menuMessage()
 			fmt.Scan(&option_choose)
 		} else {
 			option_valid = !option_valid
 		}
 	}
-
 }
 
+func menuMessage() {
+	fmt.Println("============================")
+	fmt.Println("====   Selamat Datang   ====")
+	fmt.Println("============================\n")
 
+	fmt.Println("Menu opsi :")
+	fmt.Println("1. Lihat data pasien\n2. Masukkan data pasien\n")
+	fmt.Print("Pilih opsi : ")
+}
